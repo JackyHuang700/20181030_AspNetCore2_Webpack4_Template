874eb7deb60eca8caefdbeb39a9049647ce1bab8
Jacky <jacky8595125@gmail.com>
2018-10-30 12:17:39 +0800

    ...

--- AspNetCore2_Webpack4_Template/webpack.config.js
+++ AspNetCore2_Webpack4_Template/webpack.config.js
@@ -1,9 +1,45 @@
 const path = require('path')
 
 module.exports = {
-  entry: './ClientApp/js/index.js',
+  // entry: './ClientApp/js/index.js',
+  // output: {
+  //   filename: 'main.js',
+  //   path: path.resolve(__dirname, 'wwwroot/webpackTest')
+  // },
+
+  entry: {
+    index: './ClientApp/js/index.js'
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'wwwroot/webpackTest')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv)$/,
+
+        use: ['csv-loader']
+      },
+
+      {
+        test: /\.xml$/,
+
+        use: ['xml-loader']
+      }
+    ]
   }
 }

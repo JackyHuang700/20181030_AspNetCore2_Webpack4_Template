b884baa10085e33fcbdd5ea34ae15f0977f6c51b
Jacky <jacky8595125@gmail.com>
2018-10-30 11:13:30 +0800

    init

---
+++ AspNetCore2_Webpack4_Template/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCore2_Webpack4_Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

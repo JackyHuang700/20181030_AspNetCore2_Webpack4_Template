895f0a80db03371ca1afc0bdfc9ec12ee598870e
Jacky <jacky8595125@gmail.com>
2018-10-31 12:20:30 +0800

    fix webpack-dev-server

--- AspNetCore2_Webpack4_Template/webpack.dev.js
+++ AspNetCore2_Webpack4_Template/webpack.dev.js
@@ -1,10 +1,23 @@
+const path = require('path')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  // devServer: {
-  //   contentBase: './dist'
-  // }
+  devServer: {
+    // contentBase: [path.join(__dirname, 'wwwroot')],
+    // contentBase: path.resolve(__dirname, 'wwwroot'),
+    // contentBase: './wwwroot',
+    proxy: {
+      '*': {
+      // '/api': {
+        // target: 'http://localhost:57288/',
+        target: 'https://localhost:44376/',
+        secure: false
+      }
+    },
+    hot: true,
+    open: true
+  }
 })
